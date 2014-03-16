f3dd3629ce496b281308045375f5dfa864677fa8
Sven Riedel <sr@gimp.org>
2014-03-16 05:47:25 +0100

    DRY up test

--- test/redisex_resp_receiver_test.exs
+++ test/redisex_resp_receiver_test.exs
@@ -19,6 +19,15 @@ defmodule RedisExRespReceiverTest do
     { accepted_socket, client_socket }
   end
 
+  defp decode_test( sent_data, expectation ) do
+    { s1, s2 } = create_socketpair
+    :ok = :gen_tcp.send( s1, sent_data )
+    received_data = get_response( s2 )
+    :gen_tcp.close( s1 )
+    :gen_tcp.close( s2 )
+    assert received_data == expectation
+  end
+
   test "socket_pair" do
     sent_data = "foo\r\n"
     { s1, s2 } = create_socketpair
@@ -30,100 +39,33 @@ defmodule RedisExRespReceiverTest do
   end
 
   test "simple string decoding" do
-    sent_data = "+OK\r\n"
-    { s1, s2 } = create_socketpair
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    :gen_tcp.close( s1 )
-    :gen_tcp.close( s2 )
-    assert received_data == "OK"
+    decode_test( "+OK\r\n", "OK" )
   end
 
   test "integer decoding" do
-    sent_data = ":100\r\n"
-    { s1, s2 } = create_socketpair
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    assert received_data == 100
-
-    sent_data = ":-1\r\n" 
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    assert received_data == -1
-    :gen_tcp.close( s1 )
-    :gen_tcp.close( s2 )
+    decode_test( ":100\r\n", 100 )
+    decode_test( ":-1\r\n", -1 )
   end
 
   test "error decoding" do
-    sent_data = "-WARNING\r\n"
-    { s1, s2 } = create_socketpair
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    :gen_tcp.close( s1 )
-    :gen_tcp.close( s2 )
-    assert received_data == { :redis_error, "WARNING" }
+    decode_test( "-WARNING\r\n", { :redis_error, "WARNING" } )
   end
 
   test "bulk string decoding" do
-    sent_data = "$-1\r\n"
-    { s1, s2 } = create_socketpair
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    assert received_data == nil
-
-    sent_data = "$0\r\n\r\n" 
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    assert received_data == ""
-
-    sent_data = "$3\r\nfoo\r\n" 
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    assert received_data == "foo"
-
-    :gen_tcp.close( s1 )
-    :gen_tcp.close( s2 )
+    decode_test( "$-1\r\n", nil )
+    decode_test( "$0\r\n\r\n", "" )
+    decode_test( "$3\r\nfoo\r\n", "foo" )
   end
 
   test "array decoding" do
-    sent_data = "*-1\r\n"
-    { s1, s2 } = create_socketpair
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    assert received_data == nil
-
-    sent_data = "*0\r\n" 
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    assert received_data == []
-
-    sent_data = "*2\r\n+foo\r\n+bar\r\n" 
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    assert received_data == [ "foo", "bar" ]
-
-    sent_data = "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    assert received_data == [ "foo", "bar" ]
-
-    sent_data = "*3\r\n:1\r\n:2\r\n:3\r\n"
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    assert received_data == [ 1,2,3 ]
-
-    sent_data = "*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$6\r\nfoobar\r\n"
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    assert received_data == [ 1,2,3,4,"foobar" ]
-
-    sent_data = "*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Foo\r\n-Bar\r\n"
-    :ok = :gen_tcp.send( s1, sent_data )
-    received_data = get_response( s2 )
-    assert received_data == [ [ 1, 2, 3 ], [ "Foo", { :redis_error, "Bar" } ] ]
-
-
-    :gen_tcp.close( s1 )
-    :gen_tcp.close( s2 )
+    decode_test( "*-1\r\n", nil )
+    decode_test( "*0\r\n", [] )
+    decode_test( "*2\r\n+foo\r\n+bar\r\n", [ "foo", "bar" ] )
+    decode_test( "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n", [ "foo", "bar" ] )
+    decode_test( "*3\r\n:1\r\n:2\r\n:3\r\n", [ 1, 2, 3 ] )
+    decode_test( "*5\r\n:1\r\n:2\r\n:3\r\n:4\r\n$6\r\nfoobar\r\n", 
+                 [ 1,2,3,4,"foobar" ] )
+    decode_test( "*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Foo\r\n-Bar\r\n",
+                 [ [ 1, 2, 3 ], [ "Foo", { :redis_error, "Bar" } ] ] )
   end
 end
