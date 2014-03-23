1d0e39548a01bafea404c8fd23b6aecab178ef38
Sven Riedel <sr@gimp.org>
2014-03-23 12:06:32 +0100

    Move redis cli bridge to test helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,18 @@
 ExUnit.start
+
+
+defmodule RedisCli do
+  @rediscli 'redis-cli -p 6333 '
+
+  def run( command ) when is_binary( command ) do
+    {:ok, command_list} = String.to_char_list( command )
+    run( command_list )
+  end
+
+  def run( command ) when is_list( command ) do
+    cmd = :lists.concat( [ @rediscli, command, '\n' ] )
+    output = :os.cmd( cmd )
+    { :ok, result } = String.from_char_list( output )
+    String.split( result )
+  end
+end
